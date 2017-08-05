600c633df44f67f03a2bed45b63191c1cc59b511
siddharthchd <siddharth.chaudhary1705@gmail.com>
2017-08-06 00:18:59 +0530

    french phrases

---
+++ app/src/main/java/com/example/siddh/basicphrases/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.siddh.basicphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTapped(View view) {
+
+        int id = view.getId();
+        String ourId = "";
+        ourId = view.getResources().getResourceEntryName(id);
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.siddh.basicphrases");
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
+    }
+}
